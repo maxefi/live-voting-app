eff153e301b728f944ea6f09b8c2f5e839f2973a
Maxim Efimov <maxefi.ndo@gmail.com>
2017-02-15 16:30:31 +0300

    Implement meta attribute to action creators
    The user clicks a vote button. A VOTE action is dispatched.
    The remote action middleware sends the action over the Socket.io connection.
    The client-side Redux store handles the action, causing the local hasVote state to be set.
    When the message arrives on the server, the serverside Redux store handles the action, updating the vote in the tally.
    The listener on the serverside Redux store broadcasts a state snapshot to all connected clients.
    A SET_STATE action is dispatched to the Redux store of every connected client.
    The Redux store of every connected client handles the SET_STATE action with the updated state from the server.

--- voting-client/src/remote_action_middleware.js
+++ voting-client/src/remote_action_middleware.js
@@ -1,5 +1,7 @@
 export default socket => store => next => action => {
-    console.log('in middleware', action);
-    socket.emit('action', action);
+    console.log('in middleware', action, store.getState().toJS());
+    if(action.meta && action.meta.remote) {
+        socket.emit('action', action, store.getState().toJS());
+    }
     return next(action);
 }
